c14f5df786fa249ed0e7965ed97c12793872c45d
david <daywiss@gmail.com>
2020-02-11 09:50:00 +0000

    fixed unset to not create empty paths

--- unset.js
+++ unset.js
@@ -1,6 +1,20 @@
-import touch from './touch'
+function unset(state={},path=[],cb=x=>x,i=0){
+  if(path.length == 0) return data
+  if(i == path.length-1){ 
+    cb(state,path[i])
+    return state
+  }
+  const head = path[i]
+  if(state[head] == null){
+    return state
+  }else{
+    state[head] = {...state[head]}
+  }
+  unset(state[head],path,cb,i+1)
+  return state
+}                        
 
 export default (state,path,data) => {
-  return touch(state,path,(obj,key)=>delete obj[key])
+  return unset(state,path,(obj,key)=>delete obj[key])
 }
 
